d9e5932961c775ddc232deab86d13d9be5f24641
weicheng@xiaomalixing.com <weicheng@xiaomalixing.com>
2018-08-28 18:23:22 +0800

    添加自定义属性

--- app/src/main/java/com/veken/linechartview/MainActivity.java
+++ app/src/main/java/com/veken/linechartview/MainActivity.java
@@ -3,6 +3,10 @@ package com.veken.linechartview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.veken.linecharviewmodule.DrawType;
+import com.veken.linecharviewmodule.LineChartBean;
+import com.veken.linecharviewmodule.LineChartView;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -24,17 +28,21 @@ public class MainActivity extends AppCompatActivity {
         if(mList==null){
             mList = new ArrayList<>();
         }
+        chartView.setDefaultTextSize(24);
         Random random = new Random();
         for(int i=0;i<7;i++){
             LineChartBean lineChartBean = new LineChartBean();
-            lineChartBean.setValue(String.valueOf(random.nextInt(10000)));
+            if(i==0||i==6){
+                lineChartBean.setValue("10000");
+            }else{
+                lineChartBean.setValue(String.valueOf(random.nextInt(10000)));
+            }
             lineChartBean.setDate(String.valueOf(i));
             mList.add(lineChartBean);
         }
         chartView.setData(mList);
         chartView.setyLableText("分钟");
-        chartView.setShowPicResource(R.mipmap.click_icon);
-        chartView.setxLableTextColor(R.color.ylable_textColor);
+        chartView.setDrawType(DrawType.DrawBackground);
     }
 
     @Override
